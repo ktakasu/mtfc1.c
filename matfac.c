c2c2ad185d203c25398736c1aa0eb0beacc82ef5
Knoppix User <knoppix@Microknoppix.(none)>
2016-05-16 17:08:03 +0900

    matrix factorization

--- matfac.c
+++ matfac.c
@@ -1,22 +1,21 @@
-/*matfac*/
+/*matrix factorization*/
+
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
 #include <stdlib.h>
 #include "matrix.c"
 
-//#define M 5
-//#define N 4
 #define K 2
  
 #define STEP 5000
 #define ALPHA 0.0002
 #define BETA 0.02
 #define THT 0.001
-#define GNU 0
 
 int M,N;
 
+//(i,j)成分のRとP*Qの差を求める
 double rating_er(double r,struct matrix *p,struct matrix *q,int i,int j){
   int k;
   double pq=0;
@@ -26,6 +25,7 @@ double rating_er(double r,struct matrix *p,struct matrix *q,int i,int j){
   return(r-pq);
 }
 
+//RとP*Qの差を求める
 double get_er(struct matrix *r,struct matrix *p,struct matrix *q){
   int i,j;
   double error=0;
@@ -42,7 +42,7 @@ double get_er(struct matrix *r,struct matrix *p,struct matrix *q){
 
 int main(void){
   int i,j,k,step;
-  char filename[20]="mataaa.txt";
+  char filename[20]="matrix1.txt";
   struct matrix *p,*q,*r;
   double err;
   FILE *gp;
@@ -52,14 +52,9 @@ int main(void){
   p=mat_test(mat_alloc(M,K),0,1,10000);
   q=mat_test(mat_alloc(K,N),0,1,10000);
 
-  mat_print(r); mat_print(p); mat_print(q);
-
-  if(GNU==1){
-    gp = popen("gnuplot -persist","w");
-    fprintf(gp,"set xrange[0:50000]\n");
-    fprintf(gp,"set yrange[0:0.1]\n");
-    fprintf(gp,"plot '-' \n");
-  }
+  printf("R\n");mat_print(r);
+  printf("P\n");mat_print(p);
+  printf("Q\n");mat_print(q);
 
   for(step=0;step<STEP;step++){
     for(i=0;i<M;i++){
@@ -76,18 +71,14 @@ int main(void){
     }
     if((step%100)==0){
       //printf("%f \n",get_er(r,p,q));
-      if(GNU==1)fprintf(gp,"%d %f\n",step,get_er(r,p,q));
       if(get_er(r,p,q)<THT){printf("step %d\n",step);break;}
     }
   }
-  printf("%f\n",get_er(r,p,q));
-  mat_print(p);mat_print(q);
-  mat_print(mat_mul(p,q));
+  printf("========================\n");
+  printf("error=%f\n",get_er(r,p,q));
+  printf("P\n"); mat_print(p);
+  printf("Q\n");mat_print(q);
+  printf("P*Q\n");mat_print(mat_mul(p,q));
 
-  if(GNU==1){
-    fprintf(gp,"e\n");
-    fprintf(gp,"quit\n");
-    pclose(gp);
-  }
   return(0);
 }
